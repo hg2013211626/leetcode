b4ab9e4c0a82a1970bdc3a4b1ebb7847269e9cff
hg2013211626 <2830800407@qq.com>
2024-01-09 02:48:15 +0800

    feat: 复制文件 src/main/java/org/example/array/Ugly.java

---
+++ src/main/java/org/example/array/Ugly.java
@@ -0,0 +1,41 @@
+package org.example.array;
+
+/**
+ * @Auther: Kenfeng
+ * @Date: 2023/01/08/下午5:55
+ * @Description: 丑数全集
+ */
+public class Ugly {
+
+    /**
+     * 263. 丑数:就是只包含质因数 2、3 和/或 5 的正整数
+     * @param n
+     * @return
+     */
+    public boolean isUgly(int n) {
+        if(n < 1) {
+            return false;
+        }
+        while (n % 5 == 0) {
+            n = n / 5;
+        }
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+        return n == 1;
+    }
+
+    /**
+     * 264. 丑数 II
+     * @param n
+     * @return
+     */
+    public int nthUglyNumber(int n) {
+        return 0;
+    }
+
+
+}
